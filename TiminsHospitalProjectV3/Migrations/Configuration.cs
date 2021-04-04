31c364be0bd2e2131275162688dcd1a1d34e64f5
journ <journ@DESKTOP-TR4FRLT>
2021-04-04 14:23:56 -0400

    Add project files.

---
+++ TiminsHospitalProjectV3/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+namespace TiminsHospitalProjectV3.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<TiminsHospitalProjectV3.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(TiminsHospitalProjectV3.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
