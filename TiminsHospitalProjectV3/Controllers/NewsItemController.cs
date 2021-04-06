c960a4a601522eba742abf4a9c97506e34d31af4
journ <journ@DESKTOP-TR4FRLT>
2021-04-06 15:22:48 -0400

    added ListView for NewsItems, as well as NewsItemController

---
+++ TiminsHospitalProjectV3/Controllers/NewsItemController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.IO;
+using TiminsHospitalProjectV3.Models;
+//using TiminsHospitalProjectV3.Models.ViewModels;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Diagnostics;
+using System.Web.Script.Serialization;
+
+namespace TiminsHospitalProjectV3.Controllers
+{
+    public class NewsItemController : Controller
+    {
+        private JavaScriptSerializer jss = new JavaScriptSerializer();
+        private static readonly HttpClient client;
+
+
+        static NewsItemController()
+        {
+            HttpClientHandler handler = new HttpClientHandler()
+            {
+                AllowAutoRedirect = false
+            };
+            client = new HttpClient(handler);
+            //change this to match your own local port number
+            client.BaseAddress = new Uri("https://localhost:44346/api/");
+            client.DefaultRequestHeaders.Accept.Add(
+            new MediaTypeWithQualityHeaderValue("application/json"));
+
+
+
+        }
+        public ActionResult List()
+        {
+            string url = "NewsItemData/GetNewsItemss";
+            HttpResponseMessage response = client.GetAsync(url).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                IEnumerable<NewsItemDto> SelectedHops = response.Content.ReadAsAsync<IEnumerable<NewsItemDto>>().Result;
+                return View(SelectedHops);
+            }
+            else
+            {
+                return RedirectToAction("Error");
+            }
+        }
+    }
+}
---
+++ TiminsHospitalProjectV3/Controllers/NewsItemDataController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Data;
+using System.Net.Http;
+using System.Web.Http;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Web.Http.Description;
+using TiminsHospitalProjectV3.Models;
+using System.Diagnostics;
+//Port Number:44346
+namespace TiminsHospitalProjectV3.Controllers
+{
+    public class NewsItemDataController : ApiController
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+        // GET: api/NewsItemData/GetNewsItems
+        [ResponseType(typeof(IEnumerable<NewsItemDto>))]
+        public IHttpActionResult GetNewsItems()
+        {
+            List<NewsItem> NewsItems = db.NewsItems.ToList();
+            List<NewsItemDto> HopDTOS = new List<NewsItemDto> { };
+
+            foreach (var NewsItem in NewsItems)
+            {
+                NewsItemDto NewNewsItem = new NewsItemDto
+                {
+                    NewsItemID = NewsItem.NewsItemID,
+                    Title = NewsItem.Title,
+                    NewsBody = NewsItem.NewsBody,
+                    NewItemDate = NewsItem.NewItemDate,
+                    NewsItemHasPic = NewsItem.NewsItemHasPic,
+                    NewsItemPicExtension = NewsItem.NewsItemPicExtension,
+                    UserID = NewsItem.UserID
+                };
+                HopDTOS.Add(NewNewsItem);
+            }
+
+            return Ok(HopDTOS);
+        }
+    }
+}
