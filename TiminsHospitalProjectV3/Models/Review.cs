63108b4542a4bf023333deda93eb7afaa88b5ebf
stevendatle <70961170+stevendatle@users.noreply.github.com>
2021-04-19 13:20:21 -0400

    Review CRUD + Initial billing system

---
+++ TiminsHospitalProjectV3/Models/Review.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TiminsHospitalProjectV3.Models
+{
+    public class Review
+    {
+        [Key]
+
+        public int ReviewID { get; set; }
+
+        public DateTime ReviewDate { get; set; }
+
+        public int ReviewRating { get; set; }
+
+        public string ReviewContent { get; set; }
+
+        //foreign key user ID here
+
+    }
+
+    public class ReviewDto
+    {
+        [Key]
+        public int ReviewID { get; set; }
+
+        [DisplayName("Review Date")]
+        public DateTime ReviewDate { get; set; }
+
+        [DisplayName("Review Rating")]
+        public int ReviewRating { get; set; }
+
+        [DisplayName("Review Content")]
+        public string ReviewContent { get; set; }
+    }
+}
