5dfafa74659c40c903149e3fabc865cd7187b998
journ <journ@DESKTOP-TR4FRLT>
2021-04-23 17:41:09 -0400

    Fixed ERror

--- TiminsHospitalProjectV3/Models/ViewModels/listNewsItems.cs
+++ TiminsHospitalProjectV3/Models/ViewModels/listNewsItems.cs
@@ -1,17 +1,14 @@
-﻿
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-
 namespace TiminsHospitalProjectV3.Models.ViewModels
 {
-    public class ListEvents
+    public class listNewsItems
     {
         public bool isadmin { get; set; }
 
-        public IEnumerable<EventDto> events { get; set; }
-
+        public IEnumerable<NewsItemDto> events { get; set; }
     }
 }
