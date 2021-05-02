fea5476cad02716b8aec23fed939b444c7e83f1b
Nisarg-Chauhan <46188042+Nisarg-Chauhan@users.noreply.github.com>
2021-05-02 14:58:55 -0400

    Add files via upload

---
+++ TiminsHospitalProjectV3/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Owin;
+using Owin;
+using TiminsHospitalProjectV3.Models;
+
+[assembly: OwinStartupAttribute(typeof(TiminsHospitalProjectV3.Startup))]
+namespace TiminsHospitalProjectV3
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);         
+        }
+    }
+}
