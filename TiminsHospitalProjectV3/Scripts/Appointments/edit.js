5e50e857e41413a27e91ca79b22136b95b3c8857
Nisarg-Chauhan <46188042+Nisarg-Chauhan@users.noreply.github.com>
2021-05-02 14:55:13 -0400

    Add files via upload

---
+++ TiminsHospitalProjectV3/Scripts/Appointments/edit.js
@@ -0,0 +1,17 @@
+﻿$(document).ready(function () {
+    let config = {
+        altInput: true,
+        altFormat: "Y-m-d H:i",
+        allowInput:true,
+        dateFormat: "Y-m-d H:i",
+        minDate: new Date(),
+        enableTime: true,
+        time_24hr: true,
+        minTime: "08:00",
+        maxTime: "17:30",       
+
+    }
+    $("#RequestDatetime").flatpickr(config);
+    
+    console.log($("#RequestDatetime").val());
+});
